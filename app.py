48adb7012d3d15f9f0268212373357cfc5dc4588
Raghav Maheshwari <raghav.ddps2@gmail.com>
2019-09-06 05:00:59 +0530

    Handling the user login part

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from data import Articles
 from flask_mysqldb import MySQL
 from wtforms import Form,StringField,TextAreaField,PasswordField,validators
 from passlib.hash import sha256_crypt
-
+from functools import wraps
 #get an instance of the flask class.
 app = Flask(__name__)
 
@@ -37,6 +37,7 @@ def about():
 def articles():
     return render_template('articles.html',articles=art)
 
+#Single article.
 @app.route('/article/<string:id>/')
 def article(id):
     return render_template('articles.html',id=id)
@@ -82,6 +83,75 @@ def register():
         redirect(url_for('index'))      
     return render_template('register.html',form=form)
 #This runs if the app is same as the main
+
+#User login
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+
+        #Get for fields.
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        # Create a cursor 
+        cur = mysql.connection.cursor()
+        #Get user by username.
+        result = cur.execute("SELECT * FROM users WHERE username = %s",[username])
+        if result >0:
+            #Getting the stored hash.
+            data = cur.fetchone() #This will get the first one.
+            password = data['password'] #This will give us the password.
+
+            #Compare the passwords.
+            #This is to verify
+            if sha256_crypt.verify(password_candidate,password):
+                #Lets say that can login.
+                #When we login we have to create a user session.
+                
+                #The below is the comment as to see what to do, if things do not work.
+
+                # app.logger.info('PASSWORD MATCHED')
+                #We will start the session
+                session['logged_in'] = True
+                session['username'] = username
+                flash('You are now logged in','success')
+                return redirect(url_for('dashboard'))
+
+            else:
+                error = "Invalid Login"
+                return render_template('login.html',error=error)
+        
+        else:
+            error = "Username not found"
+            return render_template('login.html',error=error)
+        
+    return render_template('login.html')
+
+
+def is_logged_in(f):
+    #Have to figure out what the heck does this wraps do!!
+    @wraps(f)
+    def wrap(*args,**kwargs):
+        if 'logged_in' in session:
+            return f(*args,**kwargs)
+        else:
+            flash('Unauthorized, Please login','danger')
+            return redirect(url_for('login'))
+    return wrap
+#Logout.
+@app.route('/logout')
+def logout():
+    session.clear() #We just have to clear the session.
+    flash('You are now logged out','success')
+    return redirect(url_for('login'))
+
+
+#Dashboard.
+@app.route('/dashboard')
+@is_logged_in
+def dashboard():
+    return render_template('dashboard.html')
+
 if __name__ == '__main__':
     app.secret_key = 'secret_123'
     app.run(debug=True) #Setting debug as true, basically, we don't have to restsrat the server.
